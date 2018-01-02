25dcc630db63bc7e56f4b0db6a92cfe0a16093b6
Alexey <33495117+Avigdor-Kolonimus@users.noreply.github.com>
2018-01-02 22:10:59 +0200

    need to complete the filter panel, change show information in IOpanel and threads/

---
+++ src/Ex3/HelpFunctionEx3.java
@@ -0,0 +1,297 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex3;
+//libraries
+import Ex1.DataWIFI;
+import Ex1.Location;
+import Ex1.WIFI;
+import Ex2.ALGOtwoCLASS;
+import Ex2.ClassOfAlgorithm1;
+import Ex2.HelpFunctions;
+import com.csvreader.CsvWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+/**
+ * help function for Assignment 3
+ * @author Alexey Titov   and   Shalom Weinberger
+ * @version 1
+ */
+public class HelpFunctionEx3 
+{
+    //variables
+    private final String pathDataBase="database//database.csv";     //path to database
+    private ReadFunctions rf=new ReadFunctions();
+    private WriteFunctions wf=new WriteFunctions();
+    //this function clears the database
+    public void Clear()
+    {
+        File myFile = new File(pathDataBase);
+        if (myFile.exists()){
+            myFile.delete();
+            try {
+                // use FileWriter constructor that specifies open for appending
+                CsvWriter tmp = new CsvWriter(new FileWriter(myFile, false), ',');	
+                //headers for first row
+                tmp.write("TIME");          tmp.write("ID");
+                tmp.write("latitude");      tmp.write("longitude");         tmp.write("altitude");
+                tmp.write("#WiFi networks");
+		for (int i=1;i<11;i++)
+		{
+                    tmp.write("SSID"+i);
+                    tmp.write("MAC"+i);
+                    tmp.write("Frequency"+i);
+                    tmp.write("Signal"+i);
+		}
+		tmp.endRecord();
+		tmp.close();
+            }catch (IOException e){
+                JOptionPane.showMessageDialog(null,"database not cleared");
+                return;
+            }
+        }
+        JOptionPane.showMessageDialog(null,"database cleared");
+    }
+    //this function writes database in kml format
+    public String SaveKML()
+    {
+        ArrayList<DataWIFI> tmp=rf.ReadCSV(pathDataBase);
+        try {
+            return wf.WriteKML(tmp);
+	}catch (IOException e) {
+            return "KML File&";
+        }
+    }
+    //the function browse a folder with the Wigle files
+    public String BrowseFolder()
+    {
+        //variable
+        int ret;
+        //File chooser
+        JFileChooser fileopen = new JFileChooser();
+        //only directory
+        fileopen.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileopen.setAcceptAllFileFilterUsed(false);
+        //dialog box for determining the desired directory
+        ret=fileopen.showDialog(null, "Open directory");
+        //check if directory is not selected
+        if (!(ret == JFileChooser.APPROVE_OPTION))
+        {
+            JOptionPane.showMessageDialog(null, "Directory is not selected\ngoodbye");
+            return "Name Folder";
+        }
+        return fileopen.getSelectedFile().getAbsolutePath();
+    }
+    //the function browse the Wigle file
+    public String BrowseFile()
+    {
+        //variables
+        int ret;                                        //flag for check a directory is selection
+        JFileChooser fileopen = new JFileChooser();
+        String csvFile ="";				//input csv file
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("WIGLE-WIFI CSV FILES","csv");
+        //open window
+        fileopen.setFileFilter(filter);
+        ret=fileopen.showDialog(null, "Open csv file");                
+        if (ret == JFileChooser.APPROVE_OPTION &&
+        fileopen.getSelectedFile().getAbsolutePath().substring(fileopen.getSelectedFile().getAbsolutePath().length()-3).equals("csv"))
+        {
+            csvFile=fileopen.getSelectedFile().getAbsolutePath();
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null, "file is not selected or the file type is not valid,\ngoodbye");
+            return "Name File";
+        }
+        return csvFile;
+    }
+    //the functon return data about number of WiFi and rows in database
+    public String ShowData()
+    {
+        return rf.ReadShowData();
+    }
+    //the function saves a special file
+    public String SaveFile()
+    {
+        //variables
+        File source = new File(pathDataBase);   //database
+        File dest;                              //printing way where to copy
+	String OUTcsvFile="";			//output csv file
+	//select the location of the file
+	FileNameExtensionFilter filter = new FileNameExtensionFilter("*.CSV","*.*");
+	JFileChooser fc = new JFileChooser();
+	fc.setFileFilter(filter);
+	if ( fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION ) {
+            try {
+            //user wrote at the end csv
+            if (fc.getSelectedFile().getAbsolutePath().substring(fc.getSelectedFile().getAbsolutePath().length()-4).equals(".csv"))
+                OUTcsvFile=fc.getSelectedFile().getAbsolutePath();
+            else 	//user did not wrote at the end csv
+		OUTcsvFile=fc.getSelectedFile().getAbsolutePath()+".csv";
+            }catch (Exception e ) {
+                JOptionPane.showMessageDialog(null, "an error occurred, the file was not saved.\ngoodbye");
+		return "Save File";
+            }
+	}
+	else	//user did not select a file to save
+	{
+            JOptionPane.showMessageDialog(null, "you did not select a file to save,\ngoodbye");
+            return "Save File";
+        }
+        dest=new File(OUTcsvFile);
+        try {
+            Files.copy(source.toPath(), dest.toPath());
+            return dest.toString();
+        }catch(IOException ex){
+            return "Save File";
+        }
+        }
+    //the function delete duplicate from new data
+    private ArrayList<DataWIFI> DeleteDuplicate(ArrayList<DataWIFI> newdata)
+    {
+        ArrayList<DataWIFI> result =rf.ReadDataBase();
+        for(int i=0;i<newdata.size();i++)
+        {    
+            boolean flag=false;
+            for(int j=0;j<result.size()&& !flag;j++)
+                if (result.get(j).getLla().compareLLA(newdata.get(i).getLla())==0 &&
+                    result.get(j).getID().equals(newdata.get(i).getID()) &&
+                    result.get(j).getTIME().equals(newdata.get(i).getTIME()))
+                {    
+                    newdata.remove(i);
+                    i--;
+                    flag=true;
+                }
+        }
+        return newdata;
+    }
+    //the function add data from the wigle file to database
+    public void AddFile(String filepath)
+    {
+        ArrayList<String> csvFiles=new ArrayList<>();
+        csvFiles.add(filepath);
+        ArrayList<DataWIFI> fileData= DeleteDuplicate(rf.ReadCSV(csvFiles));
+        wf.WriteCSV(fileData);
+    }
+    //the function add data from wigle folder to database
+    public void AddFolder(String folderpath)
+    {
+        //variables
+        ArrayList<String> files=new ArrayList<String>();	//list csv files
+        ArrayList<DataWIFI> fileData=new ArrayList<DataWIFI>();	//data of WiFiNetwork
+        File []fList;        
+        File F = new File(folderpath);                          //the path to the directory
+        //all files that are in the folder     
+        fList = F.listFiles();
+        //runs at the folder.
+        for(int i=0; i<fList.length; i++)           
+        {
+            String mark=fList[i].getName();  
+            //check if name is csv file
+            if(fList[i].isFile() && mark.substring(mark.length()-3).equals("csv"))
+                files.add(folderpath+"\\"+fList[i].getName());
+        }
+        //no files
+        if (files.isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "in the folder there are no csv files");
+        }
+        else
+        {
+            fileData= DeleteDuplicate(rf.ReadCSV(files));
+            wf.WriteCSV(fileData);
+        }
+    }
+    //the function makes the first algorithm
+    public Location FirstAlgorithm(String mac)
+    {
+        //variables
+        ClassOfAlgorithm1 coaFirst=new ClassOfAlgorithm1();
+        HelpFunctions hlp=new HelpFunctions();
+        Location place=new Location();
+        coaFirst=rf.ReadFirstAlgo(mac);
+        place=hlp.WriteWeight(coaFirst);
+        if (place==null)
+            return new Location();
+        return place;
+    }
+    /**
+    * the function returns a place for the three MACs according to the second algorithm 
+    * @param DWF-three MACs
+    * @param three data of three MACs
+    * @return location of three MACs
+    */
+    public Location ThreeMac(ArrayList<DataWIFI> DWF,DataWIFI three)
+    {
+        //variables
+        Location place=new Location();
+        HelpFunctions hlp=new HelpFunctions();
+        ArrayList<ALGOtwoCLASS> data=hlp.Find(DWF,three.getWiFi());
+        place= hlp.WeightAlgo2(three.getWiFi(),data);
+        if (place!=null)
+            return place;
+        return new Location();
+    }
+    /**
+    * the function returns a place for row as comb_no_gps_ts1.csv according to the second algorithm 
+    * @param DWF-database
+    * @param rowTS-row as comb_no_gps_ts1.csv
+    * @return location of row as comb_no_gps_ts1.csv
+    */
+    public Location RowTS(ArrayList<DataWIFI> DWF,String rowTS)
+    {
+        //variables
+        Location place=new Location();
+        HelpFunctions hlp=new HelpFunctions();
+        String[] row=rowTS.split(",");
+        if (row.length<10 || row.length>46)
+            return place;
+        DataWIFI tmpWIFI=new DataWIFI();
+        int cnt=0;                              //variables for check MAC,Signal
+        int max=0;                              //number of networks in the list
+        boolean flagtime;                       //flag for check time
+        //#WIFI network
+        try{
+            max=Integer.parseInt(row[5]);
+        }catch(NumberFormatException e){
+            System.out.println("Err: #WiFi network is no correct");	
+            return place;
+        }
+        tmpWIFI.setLla(place);
+        flagtime=tmpWIFI.setTIME(row[0]);
+        //check if time is correct
+        if (!flagtime)
+            return place;
+        tmpWIFI.setID(row[1]);
+        //read WiFi data
+        for (int i=0;i<max;cnt=0,i++)
+        {
+            WIFI tmpWF=new WIFI();
+            try {
+                cnt+=tmpWF.setMAC(row[7+i*4]);
+                tmpWF.setSSID(row[6+i*4]);
+                cnt+=tmpWF.setSignal((int) Double.parseDouble(row[9+i*4]));
+                cnt+=tmpWF.setFrequency(Integer.parseInt(row[8+i*4]));
+                if (cnt!=3)
+                    continue;
+                tmpWIFI.setWiFi(tmpWF);
+            }catch(NumberFormatException e){
+                System.out.println("Err,WIFI");
+                continue;
+            }
+        }
+        ArrayList<ALGOtwoCLASS> data=hlp.Find(DWF,tmpWIFI.getWiFi());
+        place= hlp.WeightAlgo2(tmpWIFI.getWiFi(),data);
+        if (place!=null)
+            return place;
+        return new Location();
+    }
+}
---
+++ src/Ex3/WriteFunctions.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ex3;
+//libraries
+import Ex1.DataWIFI;
+import Ex1.Location;
+import Ex1.WIFI;
+import com.csvreader.CsvWriter;
+import de.micromata.opengis.kml.v_2_2_0.AltitudeMode;
+import de.micromata.opengis.kml.v_2_2_0.Coordinate;
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Folder;
+import de.micromata.opengis.kml.v_2_2_0.Icon;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.LineStyle;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Style;
+import java.awt.HeadlessException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
+/**
+ * The write functions for help function of Assignment 3
+ * @author Alexey Titov   and   Shalom Weinberger
+ * @version 1
+ */
+public class WriteFunctions {
+    //variable
+    private final String pathDataBase="database//database.csv";     //path to database
+    /**
+     * the function generates and set a placemark object, with the given statistical data
+     * @param document structure of the KML file
+     * @param folder to add wifi data
+     * @param date time when done check
+     * @param place location of wifi mark
+     * @param dwf wifi data of wifi mark 
+     */
+    private void createPlacemarkWithChart(Document document, Folder folder,String date,Location place,WIFI dwf)
+    {
+        try {
+            Placemark placemark = folder.createAndAddPlacemark();
+            //name of mark and style
+            placemark.withName(dwf.getSSID()).withStyleUrl("#style_place")
+            //description of mark
+            .withDescription("MAC: "+dwf.getMAC()+"<br/>Frequency: <b>"+dwf.getFrequency()+"</b><br/>Signal: <b>"+dwf.getSignal()+"</b>");
+            //set coordinates
+            placemark.createAndSetPoint().addToCoordinates(place.getLon(),place.getLat(),place.getAlt());
+            //set when
+            date=date.replace(' ', 'T')+'Z';
+            placemark.createAndSetTimeStamp().setWhen(date);
+	}catch(NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Why null????");
+	}
+    }
+    /**
+     * the function generates and set a placemark object, with the given statistical data
+     * @param document structure of the KML file
+     * @param folder to add time line
+     * @param dwf list wifi data of wifi mark 
+     */
+    private void createLine(Document document, Folder folder,ArrayList<DataWIFI> dwf)
+    {
+        try {
+            //variables
+            Placemark placemark = folder.createAndAddPlacemark();
+            Style style=new Style();
+            LineStyle ls=new LineStyle();
+            List<Coordinate> coordinates=new ArrayList<>();
+            //set style
+            ls.setWidth(2);
+            ls.setColor("ff0ff0ff");
+            style.setLineStyle(ls);
+            //set coordinates
+            for(int i=0;i<dwf.size();i++)
+            {
+		Coordinate coordinate = new Coordinate(dwf.get(i).getLla().getLon(),dwf.get(i).getLla().getLat(),dwf.get(i).getLla().getAlt());
+		coordinates.add(coordinate);
+            }
+            //add name
+            placemark.withName("Time Line");
+            //add style
+            placemark.createAndAddStyle().setLineStyle(ls);
+            //add line string
+            placemark.createAndSetLineString().withTessellate(true).withCoordinates(coordinates).withAltitudeMode(AltitudeMode.CLAMP_TO_GROUND);
+        }catch(NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Why null????");
+        }
+    }
+    /**
+     * the function write kml file
+     * @param dwf list of data for kml file
+     * @return path of kml file
+     * @throws java.io.IOException dwf is null
+     */
+    public String WriteKML(ArrayList<DataWIFI> dwf) throws IOException
+    {
+        //variables
+        String OUTkmlFile="";		//output kml file
+        final Kml kml = new Kml();
+        Document doc = kml.createAndSetDocument().withName("WIFI").withOpen(true);
+        //create a Folder
+        Folder folder = doc.createAndAddFolder();
+        folder.withName("WiFi Signal").withOpen(true);
+        //select the location of the file
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.KML","*.*");
+        JFileChooser fc = new JFileChooser();
+        fc.setFileFilter(filter);
+        if ( fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION ) {
+            try {
+            	//user wrote at the end kml
+            	if (fc.getSelectedFile().getAbsolutePath().substring(fc.getSelectedFile().getAbsolutePath().length()-4).equals(".kml"))
+                    OUTkmlFile=fc.getSelectedFile().getAbsolutePath();
+            	else	//user did not wrote at the end kml
+                    OUTkmlFile=fc.getSelectedFile().getAbsolutePath()+".kml";
+            }
+            catch (Exception e ) {
+            	JOptionPane.showMessageDialog(null, "an error occurred, the file was not saved.\ngoodbye");
+    	    	return "KML File";
+            }
+        }
+        else	//user did not select a file to save
+        {
+            JOptionPane.showMessageDialog(null, "you did not select a file to save,\ngoodbye");
+            return  "KML File";
+        }
+        //create placemark elements
+        try {
+            Icon icon = new Icon()
+            .withHref("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
+            Style style = doc.createAndAddStyle();
+            style.withId("style_place") // set the stylename to use this style from the placemark
+            .createAndSetIconStyle().withScale(1.0).withIcon(icon); // set size and icon
+            style.createAndSetLabelStyle().withColor("ff0ff0ff").withScale(1.0); // set color and size of the wifi name
+            //wifi data
+	    for (int j=0;j<dwf.size();j++)
+	    {
+                int counter=dwf.get(j).getWIFINetwork();
+	        int i=0;
+	        while (counter!=0)
+	        {
+                    //create placemark for wifi
+	            try {
+                        createPlacemarkWithChart(doc,folder,dwf.get(j).getTIME(),dwf.get(j).getLla(),dwf.get(j).getWiFi().get(i));
+	            }catch(IndexOutOfBoundsException e){
+                        System.out.println("Err");
+	            }
+	            counter--;
+	            i++;
+	        }
+	    }
+	    createLine(doc,folder,dwf);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "kml record not saved");
+            return "KML File";
+    	}
+        //print and save
+        kml.marshal(new FileWriter(OUTkmlFile,false));
+        JOptionPane.showMessageDialog(null, "kml record saved");
+        return OUTkmlFile;
+    }
+    /**
+    * the function write csv file
+    * @param dwf list of data for csv file
+    */
+    public void WriteCSV(ArrayList<DataWIFI> dwf)
+    {
+        try {
+            //use FileWriter constructor that specifies open for appending
+            CsvWriter csvOutput = new CsvWriter(new FileWriter(pathDataBase, true), ',');	
+            //write out a few rows
+            for(int i=0;i<dwf.size();i++)              //write rows of data wifi
+            {
+                String[] tmp=dwf.get(i).toString().split(",");
+                csvOutput.writeRecord(tmp);
+            }
+            csvOutput.close();
+            JOptionPane.showMessageDialog(null, "csv record saved");
+        }catch(IOException | HeadlessException e) {
+            JOptionPane.showMessageDialog(null, "csv record not saved");
+        }
+    }
+}
